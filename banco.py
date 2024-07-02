fa84068742002c38beb1fcb03701d1ca09d999c7
weber silva <webersilva555@gmail.com>
2024-07-02 10:08:36 -0300

    first commit

---
+++ banco.py
@@ -0,0 +1,32 @@
+from typing import List
+from time import sleep
+
+from models.cliente import Cliente
+from models.conta import Conta
+
+contas: List[Conta] = []
+
+def main() -> None:
+    menu()
+
+def menu() -> None:
+    pass
+
+def criar_conta() -> None:
+    pass
+
+def efetuar_saque() -> None:
+    pass
+
+def efetuar_transferencia() -> None:
+    pass
+
+def listar_contas() -> None:
+    pass
+
+def buscar_conta_por_numero(numero: int) -> Conta:
+    pass
+
+if __name__=='__main__':
+    main()
+
