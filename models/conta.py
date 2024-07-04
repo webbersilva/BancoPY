b2edfee6b55417c585b1deb4d982629874059a3f
weber silva <webersilva555@gmail.com>
2024-07-03 22:28:38 -0300

    third

--- models/conta.py
+++ models/conta.py
@@ -30,7 +30,7 @@ class Conta:
         return self.__saldo
 
     @saldo.setter
-    def saldo(self: object, calor: float) -> None:
+    def saldo(self: object, valor: float) -> None:
         self.__saldo = valor
 
     @property
@@ -45,16 +45,50 @@ class Conta:
     def saldo_total(self: object) -> float:
         return self.__saldo_total
 
+    @saldo_total.setter
+    def _calcula_salto_total(self: object, valor: float) -> None:
+        self.__saldo_total = valor
+
     @property
     def _calcula_saldo_total(self: object) -> float:
         return self.saldo + self.limite
 
-    def depositar(selfself: object, valor: float) -> None:
-        pass
+    def depositar(self: object, valor: float) -> None:
+        if valor > 0:
+                self.saldo = self.saldo + valor
+                self.saldo_total = self._calcula_saldo_total
+        else:
+            print('Error ao efetuar déposito. Tente novamente')
+
 
     def sacar(self: object, valor: float) -> None:
-        pass
-
-    def transferir(self: object, destino: Conta, valor: float) -> None:
-        pass
+        if valor > 0 and self.saldo_total >= valor:
+            if self.saldo >= valor:
+                self.saldo = self.saldo -valor
+                self.saldo_total = self._calcula_saldo_total
+            else:
+                restante: float = self.saldo - valor
+                self.limite = self.limite + restante
+                self.saldo = 0
+                self.saldo_total = self._calcula_saldo_total
+                print('Saque efetuado com sucesso')
+        else:
+            print('Saque não realizado. Tente novamente')
+    def transferir(self: object, destino: object, valor: float) -> None:
+        if valor > 0 and self.saldo_total >= valor:
+            if self.saldo >= valor:
+                self.saldo = self.saldo - valor
+                self.saldo_total = self._calcula_total
+                destino.saldo = destino.saldo + valor
+                destino.saldo_total = destino._calcula_saldo_total
+            else:
+                restante: float = self.saldo - valor
+                self.saldo = 0
+                self.limite = self.limite + restante
+                self.saldo_total = self.calcula_saldo_total
+                destino.saldo = destino.saldo + valor
+                destino.saldo_total = destino._calcula_saldo_total
+            print('Trasnferência realizada co sucesso.')
+        else:
+            print('Transferência não realizada. tente novamente')
 
